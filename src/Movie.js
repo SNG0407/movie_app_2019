680d33d26554fc34ba8f3cffa046ea6411ab351c
kim <ksc0407@khu.ac.kr>
2021-02-28 19:55:13 +0900

    rating 없으면 안나오게 만들고 사이트에 올리기

--- src/Movie.js
+++ src/Movie.js
@@ -3,22 +3,26 @@ import PropTypes from "prop-types";
 import "./Movie.css";
 
 function Movie({id, year, title, summary, poster, genres, rating}) {
-    return (
-    <div className="movie">
-        <img src={poster} alt={title} title={title} />
-        <div className="movie__data">
-            <h3 className="movie__title">{title.slice(0,40)}</h3>
-            <h5 className="movie__rating">rate : {rating}</h5>
-            <h5 className="movie__year">{year}</h5>
-            <ul className="genres">
-                {genres.map((genre, index) =>(
-                    <li key={index} className="genres__genre">{genre}</li>
-                ))}
-            </ul>
-            <p className="movie__summary">{summary}</p>
-        </div>
-    </div>
-    );
+    if(rating){
+        return (
+            <div className="movie">
+                <img src={poster} alt={title} title={title} />
+                <div className="movie__data">
+                    <h3 className="movie__title">{title.slice(0,40)}</h3>
+                    <h5 className="movie__rating">rate : {rating}</h5>
+                    <h5 className="movie__year">{year}</h5>
+                    <ul className="genres">
+                        {genres.map((genre, index) =>(
+                            <li key={index} className="genres__genre">{genre}</li>
+                        ))}
+                    </ul>
+                    <p className="movie__summary">{summary}</p>
+                </div>
+            </div>
+            );
+    } else{
+        return "";
+    }   
 }
 
 
